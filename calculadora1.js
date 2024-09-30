3895d69b8378fc440b1caca8564a7f82945ddc09
DLEGATOR <zx19student393@colegiovalledelmiro.es>
2024-09-30 09:45:05 +0200

    version resta

---
+++ calculadora1.js
@@ -0,0 +1,9 @@
+function calculadora(){
+    let num1 = parseInt(prompt("ingresa un numero"));
+    let num2 = parseInt(prompt("ingresa un numero"));
+
+    let resultado = num1 - num2;
+
+    alert((resultado));
+}
+calculadora();
